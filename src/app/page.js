a0c7537ef67dbed1c34263bb170d1d0336844b91
Sparsh Chaudhary <work.sparsh01@gmail.com>
2025-01-29 19:53:51 +0100

    Check

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-black w-full h-screen">
-      <h1 className="text-4xl font-bold text-center mt-20">Hello World</h1>
+      <h1 className="text-4xl font-bold text-center mt-20">Hello World 2</h1>
     </main>
   );
 }
