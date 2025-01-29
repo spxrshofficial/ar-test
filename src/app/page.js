254fc16993e611420a795134a4d2d29c1d5c9ac8
Sparsh Chaudhary <work.sparsh01@gmail.com>
2025-01-29 21:51:59 +0100

    Aframe

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,23 @@
+'use client';
+import { Canvas } from "@react-three/fiber";
+import 'aframe';
 
 export default function Home() {
+  
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-black w-full h-screen">
-      <h1 className="text-4xl font-bold text-center mt-20">Hello World 2232423</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between bg-gray w-full h-screen">
+      {/* <Canvas className="w-full h-full">
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <mesh rotation={[-Math.PI / 3, 0, 0]} position={[0, 0, 0]}>
+          <boxGeometry />
+          <meshNormalMaterial />
+        </mesh>
+      </Canvas> */}
+      <a-scene embedded>
+        <a-box color="#f0cd21" position="0 1 -4" rotation="0 90 0"></a-box>
+      </a-scene>
     </main>
   );
 }
